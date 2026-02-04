59e984ade326a76e5809278f853bc71283aa8b76
GuilhermeArduiniPy <guilhermeardn1@hotmail.com>
2026-02-04 20:08:21 -0300

    Criando as factory e seeders das campanhas, template.

--- app/Models/Campaign.php
+++ app/Models/Campaign.php
@@ -6,14 +6,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Template extends Model
+class Campaign extends Model
 {
-
+    /** @use HasFactory<\Database\Factories\CampaignFactory> */
     use HasFactory;
     use SoftDeletes;
-
-    protected $fillable = [
-        'name',
-        'body',
-    ];
 }
